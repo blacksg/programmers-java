874debe6c96a14f9f883bd59196c14d9663436ca
blacksg <sagwa.nth@gmail.com>
2023-05-29 02:42:03 +0900

    Add solutions * [Lv.1] 연습문제 - 문자열 다루기 기본 * [Lv.1] 완전탐색 - 최소직사각형 * [Lv.1] 월간 코드 챌린지 시즌2 - 약수의 개수와 덧셈

---
+++ src/main/java/level1/BasicStringHandling.java
@@ -0,0 +1,17 @@
+package level1;
+
+/*
+ * 연습문제 - 문자열 다루기 기본 (Lv.1)
+ *
+ * [요구사항]
+ * - 문자열 `s`의 길이가 4 또는 6이며 숫자로만 구성되어 있는지 여부를 리턴한다.
+ *
+ * [제한사항]
+ * - 1 ≤ `s`의 길이 ≤ 8
+ * - `s`는 알파벳 또는 숫자로 이루어져 있다.
+ */
+class BasicStringHandling {
+    public boolean solution(String s) {
+        return (s.length() == 4 || s.length() == 6) && s.matches("\\d+");
+    }
+}
---
+++ src/main/java/level1/DivisorsAndAddition.java
@@ -0,0 +1,47 @@
+package level1;
+
+/*
+ * 월간 코드 챌린지 시즌2 - 약수의 개수와 덧셈 (Lv.1)
+ *
+ * [요구사항]
+ * - 주어진 정수 `left`와 `right` 사이의 정수들에 대해
+ *   약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 결과를 리턴한다.
+ *
+ * [제한사항]
+ * - 1 ≤ `left` ≤ `right` ≤ 1,000
+ */
+class DivisorsAndAddition {
+    // 각 자연수에 대해 약수의 개수를 하나씩 세는 풀이
+    public int solution1(int left, int right) {
+        int result = 0;
+        for (int num = left; num <= right; num++) {
+            int divisorCount = countDivisorsOf(num);
+            result += (divisorCount % 2 == 0) ? num : -num;
+        }
+        return result;
+    }
+    
+    // 자연수의 약수의 개수를 계산하는 메서드
+    private int countDivisorsOf(int num) {
+        int count = 0;
+        for (int d = 1; d * d <= num; d++) {
+            if (num % d == 0) {
+                count++;
+                if (d * d != num) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    // 완전제곱수의 특성을 이용한 풀이
+    public int solution2(int left, int right) {
+        int result = 0;
+        for (int num = left; num <= right; num++) {
+            int sqrt = (int) Math.sqrt(num);
+            result += (sqrt * sqrt == num) ? -num : num;
+        }
+        return result;
+    }
+}
